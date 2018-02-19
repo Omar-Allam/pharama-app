c5d2c645ed73ec3c190d96ead4616b150271ae69
omar <omar.coder007@gmail.com>
2018-02-19 21:05:50 +0300

    update image size

--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,31 @@
 |
 */
 
-Route::resource('medicine','MedicineController');
 
-Route::get('/', function () {
-    return view('index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('medicine','MedicineController');
+
+    Route::get('/', function () {
+        return view('index');
+    });
+
+
+    Route::get('get-types',function(){
+        $types = \App\Type::all();
+        return ["types"=>$types];
+    });
+
+    Route::get('get-companies',function(){
+        $companies = \App\Company::all();
+        return ["companies"=>$companies];
+    });
+
+    Route::get('get-medicines',function(){
+        $medicines = \App\Medicine::all();
+        return ["medicines"=>$medicines];
+    });
+
+
+    Route::get('/home', 'HomeController@index')->name('home');
 });
-
-
-Route::get('get-types',function(){
-    $types = \App\Type::all();
-   return ["types"=>$types];
-});
-
-Route::get('get-companies',function(){
-    $companies = \App\Company::all();
-   return ["companies"=>$companies];
-});
-
-Route::get('get-medicines',function(){
-    $medicines = \App\Medicine::all();
-   return ["medicines"=>$medicines];
-});
+Auth::routes();
